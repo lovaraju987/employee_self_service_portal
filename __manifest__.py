860a0e6ec069e9a67e2a7d5b62ea184c032e4401
lovaraju.mylapalli <mailapallilovaraaju987@gmail.com>
2025-05-22 18:10:13 +0530

    employee_self_service portal odoo

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# __manifest__.py
+{
+    "name": "Employee Self Service Portal",
+    "version": "1.0",
+    "depends": ["portal", "hr", "hr_attendance", "hr_payroll", "hr_holidays"],
+    "category": "Human Resources",
+    "summary": "Allow employees to access and manage their information via portal access.",
+    "data": [
+        "security/ir.model.access.csv",
+        "security/portal_employee_security.xml",
+        "views/menu.xml",
+        "views/portal_layout.xml",
+        "views/portal_employee_templates.xml",
+        "views/portal_attendance_templates.xml",
+        "data/portal_data.xml"
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+}
---
+++ controllers/main.py
@@ -0,0 +1,38 @@
+# controllers/main.py
+from odoo import http
+from odoo.http import request
+
+class PortalEmployee(http.Controller):
+
+    @http.route('/my/employee', type='http', auth='user', website=True)
+    def portal_employee_profile(self, **kwargs):
+        employee = request.env['hr.employee'].sudo().search([('user_id', '=', request.uid)], limit=1)
+        return request.render('employee_self_service_portal.portal_employee_profile', {'employee': employee})
+
+    @http.route('/my/employee/attendance/checkin', type='http', auth='user', methods=['POST'], website=True)
+    def check_in(self, **post):
+        employee = request.env['hr.employee'].sudo().search([('user_id', '=', request.uid)], limit=1)
+        request.env['hr.attendance'].sudo().create({
+            'employee_id': employee.id,
+            'check_in': fields.Datetime.now(),
+        })
+        return request.redirect('/my/employee')
+
+    @http.route('/my/employee/attendance/checkout', type='http', auth='user', methods=['POST'], website=True)
+    def check_out(self, **post):
+        employee = request.env['hr.employee'].sudo().search([('user_id', '=', request.uid)], limit=1)
+        last_attendance = request.env['hr.attendance'].sudo().search([('employee_id', '=', employee.id)], order='check_in desc', limit=1)
+        if last_attendance and not last_attendance.check_out:
+            last_attendance.check_out = fields.Datetime.now()
+        return request.redirect('/my/employee')
+    
+    @http.route('/my/employee/attendance', type='http', auth='user', website=True)
+    def portal_attendance_history(self, **kwargs):
+        employee = request.env['hr.employee'].sudo().search([('user_id', '=', request.uid)], limit=1)
+        attendances = request.env['hr.attendance'].sudo().search([
+            ('employee_id', '=', employee.id)
+        ], order='check_in desc', limit=20)
+        return request.render('employee_self_service_portal.portal_attendance', {
+            'attendances': attendances,
+            'employee': employee,
+        })
